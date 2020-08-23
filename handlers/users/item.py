4c3fdb6870f284e15000467e48eb18c9d9480130
KrissLa <arsavit@gmail.com>
2020-08-23 22:26:47 +0300

    removed unnecessaryЭ

--- handlers/users/item.py
+++ handlers/users/item.py
@@ -1,5 +1,3 @@
-import logging
-import re
 from aiogram import types
 from aiogram.types import CallbackQuery
 
--- keyboards/inline/item_buttons.py
+++ keyboards/inline/item_buttons.py
@@ -4,16 +4,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from keyboards.inline.callback_datas import item_callback
 
 
-def get_count_likes():
-    item_likes = open('item_likes_1.txt', 'r')
-    likes_count = item_likes.read()
-    item_likes.close()
-    return likes_count
-
-
-item_likes = open('item_likes_1.txt', 'r').read()
-item_dislikes = open('item_dislikes_1.txt', 'r').read()
-
 item_markup = InlineKeyboardMarkup(row_width=2,
                                    inline_keyboard=[
                                        [
